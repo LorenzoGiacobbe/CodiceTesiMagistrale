5d642b316b5ea1e0275cce0ca9d8ed81a0f89416
LorenzoGiacobbe <lorenzo.gcb@gmail.com>
2022-06-07 13:00:50 +0200

    added correlation calculation between bit difference and output  and between input and output

--- correlations/correlations.py
+++ correlations/correlations.py
@@ -5,6 +5,13 @@ import sys
 import os
 import math
 
+def diff(s1, s2):
+	count = 0
+	for i in range(len(s1)):
+		if s1[i] != s2[i]:
+			count = count +1
+	return count
+
 # User interaction
 def get_inputs():
 	tb = input("insert the path of testbench to execute:")
@@ -43,17 +50,19 @@ def create_input_list(len):
     il = list()
     pre = list()
     post = list()
+    bit_diff = list()
 
     for i in range(0, len):
-        s = str(format(i, '04b'))
+        s = format(i, '04b')
         il.append(s)
 
-    for i in range(0, len):
-        for j in range(0, len):
-            pre.append(il[i])
-            post.append(il[j])
-
-    return pre, post
+    for i in range(len):
+        for j in range(len):
+            pre.append(i) #il[i])
+            post.append(j) #il[j])
+            bit_diff.append(diff(il[i], il[j]))
+            
+    return pre, post, bit_diff
 
 def create_corr_table(log):
 
@@ -61,19 +70,7 @@ def create_corr_table(log):
     t_len = math.sqrt(len(toggles))
     inputs = create_input_list(int(t_len))
 
-    data = {"starting input": inputs[0], "new input": inputs[1], "number of toggles": toggles}
-
-    df = pd.DataFrame(data = data)
-    
-    return df
-    
-def new_create_corr_table(log):
-
-    toggles = create_toggle_list(log)
-    t_len = math.sqrt(len(toggles))
-    inputs = create_input_list(int(t_len))
-
-    data = {"number of toggles": toggles}
+    data = {"starting input": inputs[0], "new input": inputs[1], "diff bits": inputs[2], "number of toggles": toggles}
 
     df = pd.DataFrame(data = data)
     
@@ -93,17 +90,35 @@ def simulate(tb):
     
     return log, log_del, log_in_del
 
-def pearsons_correlation(df1, df2):
-
-    df1 = df1.iloc[:, 2].to_frame()
-    df2 = df2.iloc[:, 2].to_frame()
+def pearsons_correlation_diff(df):
     
-    df = pd.concat([df1, df2], axis=1)
+    diff = df.iloc[:, 2].to_frame()
+    toggles = df.iloc[:, 3].to_frame()
     
-    # columns = ['col1', 'col2']
+    df = pd.concat([diff, toggles], axis=1)
+    
+    # columns = ['inputs', 'toggles']
     # df.columns = columns
     
     corr_table = df.corr(method='pearson')
+    print(corr_table)
+    print("------------------------------------------------------------")
+    
+    return corr_table
+    
+def pearsons_correlation_in(df):
+    
+    inputs = df.iloc[:, 1].to_frame()
+    toggles = df.iloc[:, 3].to_frame()
+    
+    df = pd.concat([inputs, toggles], axis=1)
+    
+    # columns = ['inputs', 'toggles']
+    # df.columns = columns
+    
+    corr_table = df.corr(method='pearson')
+    print(corr_table)
+    print("------------------------------------------------------------")
     
     return corr_table
 
@@ -117,18 +132,30 @@ if __name__ == "__main__":
     #spreadsheet = ui[1]
 
     vvp_logs = simulate(tb)
-
+    
     df = create_corr_table(vvp_logs[0])
     df_del = create_corr_table(vvp_logs[1])
     df_in_del = create_corr_table(vvp_logs[2])
+    
+    print()
+    print("Correlation with inputs:")
+    corr_table = pearsons_correlation_in(df)
+    corr_table_del = pearsons_correlation_in(df_del)
+    corr_table_in_del = pearsons_correlation_in(df_in_del)
 
-    corr_table = pearsons_correlation(df, df)
+    print()
+    print("Correlation with bit changes:")
+    corr_table = pearsons_correlation_diff(df)
+    corr_table_del = pearsons_correlation_diff(df_del)
+    corr_table_in_del = pearsons_correlation_diff(df_in_del)
 
     with pd.ExcelWriter("./spreadsheets/" + spreadsheet + ".xlsx") as writer:
         df.to_excel(writer, index=False, sheet_name="without delays")
         df_del.to_excel(writer, index=False, sheet_name="gate delays")
         df_in_del.to_excel(writer, index=False, sheet_name="gate+input delays")
         corr_table.to_excel(writer, sheet_name="corr table")
+        corr_table_del.to_excel(writer, sheet_name="corr table gate delays")
+        corr_table_in_del.to_excel(writer, sheet_name="corr table gate+input delays")
         
     print("spreadsheet with results saved in " + os.getcwd() + "/spreadsheets/" + spreadsheet + ".xlsx")
 
